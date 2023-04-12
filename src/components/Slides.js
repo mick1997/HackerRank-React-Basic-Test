ce7d328d2eb708b9843820532a5a1004fd8c5af6
mick1997 <mickjack2013@gmail.com>
2023-04-12 00:36:36 -0400

    first time commit

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Slides({slides}) {
+
+    const [slide, setSlide] = useState(slides[0]);
+    const [disablePrev, setDisablePrev] = useState(true);
+    const [disableNext, setDisableNext] = useState(false);
+    const [disableRestart, setDisableRestart] = useState(true);
+
+    // find total length of slide
+    const slideMaxIndex = slides.length - 1;
+
+    useEffect(() => {
+        const curIdx = slides.findIndex((item) => item.title === slide.title);
+
+        if (curIdx === slideMaxIndex) {
+            setDisableNext(true);
+        }
+        else if (curIdx === 0) {
+            setDisablePrev(true);
+            setDisableRestart(true);
+        }
+        else if (curIdx > 0 && curIdx < slideMaxIndex) {
+            setDisableNext(false);
+            setDisablePrev(false);
+            setDisableRestart(false);
+        }
+    }, [slide]);
+
+    const pass = (numIdx) => {
+          setSlide(slides[slides.findIndex((item) => item.title === slide.title) + numIdx])
+    }
+
+    const restart = () => {
+        setDisableNext(false);
+        setDisableRestart(true);
+        setSlide(slides[0]);
+    }
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick={restart} disabled={disableRestart}>Restart</button>
+                <button data-testid="button-prev" className="small" disabled={disablePrev} onClick={() => {pass(-1)}}>Prev</button>
+                <button data-testid="button-next" className="small" disabled={disableNext} onClick={() => {pass(+1)}}>Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slide.title}</h1>
+                <p data-testid="text">{slide.text}</p>
             </div>
         </div>
     );
